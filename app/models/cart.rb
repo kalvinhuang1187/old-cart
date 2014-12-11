cdaa3f0f82ab244cb9d3fd7d4a178ad61180d512
Kalvin Huang <kalvinhuang1187@gmail.com>
2014-12-11 13:19:55 -0800

    add product to cart

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,3 +1,16 @@
 class Cart < ActiveRecord::Base
   has_many :line_items, :dependent => :destroy
+  
+  def add_product(product_id)
+    current_item = line_items.where(:product_id => product_id).first
+    #current_item = line_items.find_by_product_id(product_id)
+    if current_item
+      current_item.quantity += 1
+    else
+      current_item = LineItem.new(:product_id=>product_id)
+      line_items << current_item
+      #current_item = line_items.build(:product_id => product_id)
+    end
+    current_item
+  end
 end
