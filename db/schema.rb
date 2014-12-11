28fd339790442c41f251b40d0341807d959d9e90
Kalvin Huang <kalvinhuang1187@gmail.com>
2014-12-11 13:59:33 -0800

    group products in cart together with a quantity number

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141210193017) do
+ActiveRecord::Schema.define(version: 20141211212308) do
 
   create_table "carts", force: true do |t|
     t.datetime "created_at"
